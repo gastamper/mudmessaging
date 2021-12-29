7c1e57c6f3c77aa3d53583c01b6449234104c96e
Greg Stamper <gastamper@gmail.com>
2021-12-29 11:24:14 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# MUD messaging plugin
+This is a simple plugin designed to watch a log file and popup a notification on the desktop
+any time new data is appended (f.e. tells, shouts, etc).
+
+# Install instructions
+1. `mkdir -p ~/.local/share/gnome-shell/extensions/arctic@gastamper.github.io`
+2. `wget -o https://github.com/gastamper/mudmessaging/archive/refs/heads/master.zip && unzip master.zip -d ~/.local/share/gnome-shell/extensions/mudmessaging@gastamper.github.io && rm master.zip`
+3. Reload GNOME with `CTRL-F2`, `r`, `ENTER`.
---
+++ extension.js
@@ -0,0 +1,86 @@
+// General imports
+const {St, Gio, GLib, GObject} = imports.gi;
+const {main, panelMenu, popupMenu} = imports.ui;
+
+// For converting load_contents guint8/CUCHAR to string
+const ByteArray = imports.byteArray;
+
+// Self-reference for init/enable/disable
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+
+// Globals for calling out of functions to self
+let menu;
+let thisObj;
+
+// Extend panelMenu.Button with necessary functionality
+const Indicator = GObject.registerClass(
+class Indicator extends panelMenu.Button {
+    _init() {
+        // Init with 0 else no dimensions
+        super._init(0);
+        thisObj = this;
+
+        // Set taskbar label for extension
+        let label = new St.Label({ text: "Arctic" });
+        this.add_child(label);
+
+        // Initialize pull-down menu
+        // 1. Build log menu & text box
+        this.log = Gio.File.new_for_path("/dev/null");
+        this.menuItem = new popupMenu.PopupMenuItem('Log location:');
+        this.logLocation = new St.Label({ text: this.log.get_path() });
+        this.logLocation.clutter_text.set_reactive(true);
+        this.logLocation.clutter_text.set_editable(true);
+        this.logLocation.clutter_text.set_activatable(true);
+        this.menuItem.add_child(this.logLocation);
+        // 2. Build button to trigger reload of log file location
+        this.menu.addMenuItem(this.menuItem);
+        this.updateLog = new popupMenu.PopupMenuItem('Update log location');
+        this.updateLog.actor.connect('button_press_event', this._updateLogLocation);
+        this.menu.addMenuItem(this.updateLog);
+        // 3. Build a file monitor to watch log file for changes
+        this._buildMonitor();
+    }
+
+    _buildMonitor() {
+        // Gio.File.Monitor isn't really capable of rendering errors (can't read, etc)
+        // so the only real error checking that is possible is on load_contents.
+        this.monitor = this.log.monitor(Gio.FileMonitorFlags.NONE, null);
+        if ( ! this.monitor ) {
+            log("Couldn't create monitor for log file");
+        } else {
+            this.monitor.connect('changed', function (file, otherfile, eventType) {
+                let [success, contents] = thisObj.log.load_contents(null);
+                if (success) { 
+                    let ba = ByteArray.toString(contents);
+                    main.notify(ba);
+                } else {
+                    main.notify("Reading log file failed");
+                }
+            });
+        }
+    }
+    _updateLogLocation() {
+        thisObj.monitor.cancel();
+        thisObj.log = Gio.File.new_for_path(thisObj.logLocation.get_text());
+        thisObj._buildMonitor();
+    }
+});
+
+function init() {
+    log(`Initializing ${Me.metadata.name} version ${Me.metadata.version}`)
+}
+
+function enable() {
+    menu = new Indicator();
+    main.panel._addToPanelBox('indicator', menu, 1, main.panel._rightBox);
+}
+
+function disable() {
+    // GNOME shell extensions are required to clean up after themselves
+    if (menu != null) {
+        menu.destroy();
+        menu = null;
+    }
+}
